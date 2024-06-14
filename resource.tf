8f6bbf7551ce9cabcf3194ca422c97cd35e7a413
sudha <sudha.sivaprakasam@gmail.com>
2024-06-14 06:20:40 +0000

    test

--- resource.tf
+++ resource.tf
@@ -1,13 +1,13 @@
-#how to create a resource group. 
+/*#how to create a resource group. 
 resource "azurerm_resource_group" "myrg" { #this is the refrence it will be managed by terraform to store your logs
   name = "${local.resource_name_prefix}-${var.resource_group_name}"
   #sap-dev-rg-default
   location = var.resource_group_location
   tags     = local.common_tags
 }
-
+*/
 module "vnet" {
-  source = "github.com/sudhasivaprakasam/terraform-module/master/modules/vnet"
+  source = "github.com/sudhasivaprakasam/terraform-module/tree/master/modules/vnet"
   business_devision = "sap"
 environment = "stage"
 resource_group_name = "rg-default"
